2f56cbd38913013bd07c9d42b5e807d24a8460f4
wallasce <lwallasce@outlook.com>
2022-11-15 00:17:54 -0300

    Criando Classe Usuario

---
+++ Pedagio/src/Modelos/Usuario.java
@@ -0,0 +1,21 @@
+package Modelos;
+
+public abstract class Usuario {
+	protected Veiculo veiculo;
+	protected String cpf;
+	protected String cnh;
+	
+	public Usuario(Veiculo veiculo, String cpf, String cnh) {
+		this.veiculo = veiculo;
+		this.cpf = cpf;
+		this.cnh = cnh;
+	}
+	
+	public String ConsultarPlaca() {
+		return this.veiculo.getPlaca();
+	}
+	
+	public String ConsultarTipoVeiculo() {
+		return this.veiculo.getModelo();
+	}
+}
