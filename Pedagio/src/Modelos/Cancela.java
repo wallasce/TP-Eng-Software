d54d164e6eee249b7a63789c1099837f0e983231
wallasce <lwallasce@outlook.com>
2022-12-06 16:49:26 -0300

    Adicionando led a cancela

--- Pedagio/src/Modelos/Cancela.java
+++ Pedagio/src/Modelos/Cancela.java
@@ -2,20 +2,36 @@ package Modelos;
 
 public class Cancela {
 	private Boolean aberta;
+	private Boolean ledVerde;
+	private Boolean ledVermelho;
 	
 	public Cancela() {
 		this.aberta = false;
+		this.ledVerde = false;
+		this.ledVermelho = true;
 	}
 	
 	public boolean abrirCancela() {
 		this.aberta = true;
+		this.ledVerde = true;
+		this.ledVermelho = false;
 		
 		return this.aberta;
 	}
 	
 	public boolean fecharCancela() {
 		this.aberta = false;
+		this.ledVerde = false;
+		this.ledVermelho = true;
 		
 		return this.aberta;
+	}
+	
+	public boolean getLedVerde() {
+		return this.ledVerde;
+	}
+	
+	public boolean getLedVermelho() {
+		return this.ledVermelho;
 	}
 }
