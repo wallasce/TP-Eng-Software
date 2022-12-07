f613b3516412292a11c83a59bb75f7552ac4e869
wallasce <lwallasce@outlook.com>
2022-12-07 00:01:10 -0300

    Teste do Cartao

---
+++ Pedagio/src/Testes/TesteCartao.java
@@ -0,0 +1,20 @@
+package Testes;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import Modelos.Cartao;
+
+class TesteCartao {
+
+	@Test
+	void testeRealizarCobrança() {
+		Cartao cartao = new Cartao(12345, new Date(), 123);
+		
+		assertTrue("Deve retornar que a cobrança foi feita", cartao.realizarCobranca(25.50));
+	}
+
+}
